3545bfc582f8e0e67198b5e1bd85ffd721c50519
cj <66845682+chrispycreme420@users.noreply.github.com>
2021-05-24 22:24:22 -0400

    src ig

---
+++ src/main/java/me/earth/phobos/features/setting/Bind.java
@@ -0,0 +1,68 @@
+package me.earth.phobos.features.setting;
+
+import com.google.common.base.Converter;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import net.minecraft.client.util.InputUtil;
+
+public class Bind {
+    private int key;
+
+    public Bind(int key) {
+        this.key = key;
+    }
+
+    public static Bind none() {
+        return new Bind(-1);
+    }
+
+    public int getKey() {
+        return this.key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public boolean isEmpty() {
+        return this.key < 0;
+    }
+
+    public String toString() {
+        return this.isEmpty() ? "None" : (this.key < 0 ? "None" : this.capitalise((InputUtil.fromKeyCode(this.key,0)).toString()));
+    }
+
+    public boolean isDown() {
+        return !this.isEmpty() && InputUtil.isKeyPressed(1,this.key);
+    }
+
+    private String capitalise(String str) {
+        if (str.isEmpty()) {
+            return "";
+        }
+        return Character.toUpperCase(str.charAt(0)) + (str.length() != 1 ? str.substring(1).toLowerCase() : "");
+    }
+
+    public static class BindConverter
+            extends Converter<Bind, JsonElement> {
+        public JsonElement doForward(Bind bind) {
+            return new JsonPrimitive(bind.toString());
+        }
+
+        public Bind doBackward(JsonElement jsonElement) {
+            String s = jsonElement.getAsString();
+            if (s.equalsIgnoreCase("None")) {
+                return Bind.none();
+            }
+            int key = -1;
+            try {
+                key = Integer.parseInt(s);
+            } catch (Exception exception) {
+            }
+            if (key == 0) {
+                return Bind.none();
+            }
+            return new Bind(key);
+        }
+    }
+}
---
+++ src/main/java/me/earth/phobos/util/Util.java
@@ -0,0 +1,8 @@
+package me.earth.phobos.util;
+
+
+import net.minecraft.client.MinecraftClient;
+
+public interface Util {
+    MinecraftClient mc = MinecraftClient.getInstance();
+}
